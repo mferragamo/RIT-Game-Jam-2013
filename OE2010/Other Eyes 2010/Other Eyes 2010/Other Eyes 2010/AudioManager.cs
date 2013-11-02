3fc2a76f3a7a3277bccb59da77a8900f00ff9c96
unknown <mjf5823@GDD-19.main.ad.rit.edu>
2013-11-02 13:28:24 -0400

    Cleaned up audio manager

--- OE2010/Other Eyes 2010/Other Eyes 2010/Other Eyes 2010/AudioManager.cs
+++ OE2010/Other Eyes 2010/Other Eyes 2010/Other Eyes 2010/AudioManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region Using Statements
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +7,35 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Media;
+#endregion
 
 namespace Other_Eyes_2010
 {
     class AudioManager
     {
         #region Attributes
-        public Dictionary<string,SoundEffect> soundEffects;
-        public List<Song> backgroundSongs;
-        public ContentManager content;
-        public int songIndex = 0;
-        public static const float MUSIC_VOLUME = 0.5f;
-        public static const float SFX_VOLUME = 0.7f;
+        private Dictionary<string,SoundEffect> soundEffects;
+        private List<Song> backgroundSongs;
+        private ContentManager content;
+        private int songIndex = 0;
+        private static const float MUSIC_VOLUME = 0.5f;
+        private static const float SFX_VOLUME = 0.7f;
+        #endregion
+
+        #region Properties
+        public List<string> SoundEffects
+        {
+            get { return soundEffects.Keys.ToList(); }
+        }
         #endregion
 
         #region Constructor
+
+
+        /// <summary>
+        /// Creates an instance of the audio manager class.
+        /// </summary>
+        /// <param name="c">The game's content manager</param>
         public AudioManager(ContentManager c)
         {
             content = c;
@@ -35,19 +50,53 @@ namespace Other_Eyes_2010
         }
         #endregion
 
-        #region Functions
+        #region Public Functions
 
+        /// <summary>
+        /// Pauses the music.
+        /// </summary>
+        public void Pause()
+        {
+            if (MediaPlayer.State != MediaState.Paused)
+                MediaPlayer.Pause();
+        }
+
+        /// <summary>
+        /// Resumes the music
+        /// </summary>
+        public void Resume()
+        {
+            if (MediaPlayer.State != MediaState.Playing)
+                MediaPlayer.Resume();
+        }
+
+        /// <summary>
+        /// Basic update. Checks to see if the current song is done, and if so, plays the next one.
+        /// </summary>
         public void Update()
         {
             if (MediaPlayer.State == MediaState.Stopped)
                 PlayNextSong();
         }
-        public void LoadContent()
+
+        /// <summary>
+        /// Gets an effect and plays it.
+        /// </summary>
+        /// <param name="name">The name of the effect to play</param>
+        public void PlaySoundEffect(string name)
         {
-            backgroundSongs.Add(content.Load<Song>("heaven_descends"));
-            backgroundSongs.Add(content.Load<Song>("ringing_of_bells"));
+            SoundEffectInstance effect;
+            effect = soundEffects[name].CreateInstance();
+
+            effect.Volume = SFX_VOLUME;
+            if (effect.State != SoundState.Playing)
+                effect.Play();
         }
-        
+
+
+        /// <summary>
+        /// Stops the current song, if necessary, and plays the next one in the list.
+        /// </summary>
         public void PlayNextSong()
         {
             songIndex++;
@@ -59,16 +108,20 @@ namespace Other_Eyes_2010
 
             MediaPlayer.Play(backgroundSongs[songIndex]);
         }
+        #endregion
 
-        public void PlaySoundEffect(string name)
+        #region Private Functions
+
+        /// <summary>
+        /// Loads audio content into respective containers.
+        /// </summary>
+        private void LoadContent()
         {
-            SoundEffectInstance effect;
-            effect = soundEffects[name].CreateInstance();
-
-            effect.Volume = SFX_VOLUME;
-            if (effect.State != SoundState.Playing)
-                effect.Play();
+            backgroundSongs.Add(content.Load<Song>("heaven_descends"));
+            backgroundSongs.Add(content.Load<Song>("ringing_of_bells"));
         }
         #endregion
+
+
     }
 }
